36198440575ac621b2450ecf35344982853df1f8
Melat Bekele <27melat28@gmail.com>
2021-07-11 01:49:37 -0400

    adding files

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ Station = Base.classes.station
 
 
 app = Flask(__name__)
+
 @app.route("/")
 def Home():
     return (
@@ -48,12 +49,29 @@ def percipitation():
 @app.route("/api/v1.0/stations")
 def stations():
     session  = Session(engine)
-    stations_results = session.query(Station.station, Station.name).all()
-    return jsonify(stations_results)
+    Stations_Data = session.query(Station.station, Station.name).all()
+    return jsonify(Stations_Data)
     session.close()
 
 @app.route("/api/v1.0/tobs")
+def tobs(): 
+    session  = Session(engine)
+    recent = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    recent_date= dt.datetime.strptime(recent[0],'%Y-%m-%d')
+    yr_of_Rec_date = dt.date(recent_date.year - 1, recent_date.month, recent_date.day)
 
-@app.route("/api/v1.0/temp/<start>")
+    Station = session.query(Measurement).group_by(Measurement.station).count()
+    active_stations = session.query(Measurement.station, func.count(Measurement.id)).group_by(Measurement.station).order_by(func.count(Measurement.id).desc()).all()
+    station_active = active_stations[0][0]
 
-@app.route("/api/v1.0/temp/<start>/<end>")
+    station_stat=(session.query(func.min(Measurement.id),func.max(Measurement.id),func.avg(Measurement.id)).filter(Measurement.station == active_stations[0][0]).all())
+
+    temp = (session.query(Measurement.tobs).filter(Measurement.date>=yr_of_Rec_date).filter(Measurement.station == station_active).all())
+    return jsonify(temp)
+    session.close()
+
+
+
+if __name__ == '__main__':
+    app.run()
+
